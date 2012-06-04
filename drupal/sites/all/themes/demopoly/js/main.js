a21b83e952bddf73b7b17719028dc6d05f1f43e3
Ammerseekind <info@ammerseekind.de>
2012-06-04 18:51:10 +0200

    masonry masonry ….

--- drupal/sites/all/themes/demopoly/js/main.js
+++ drupal/sites/all/themes/demopoly/js/main.js
@@ -11,16 +11,3 @@
 })(jQuery)
 
 
-    <!----------------->
-    <!-- Grid System -->
-    <!----------------->
-     
-var $container = $('#image-container');
-$container.imagesLoaded(function(){
-  $container.masonry({
-    itemSelector : '.box,.infotext',
-    columnWidth : 1,
-	animationOptions : { queue: true, duration: 500 },
-	isAnimated : true
-  });
-});
--- drupal/sites/all/themes/demopoly/page.tpl.php
+++ drupal/sites/all/themes/demopoly/page.tpl.php
@@ -70,6 +70,47 @@ $base_path = '/'.drupal_get_path('theme', 'demopoly').'/';
 <!-- Ende Content -->           
 
 </div>
+<nav id="page-nav">
+  <a href="index.php"></a>
+</nav>
+<script>
 
 
+$(function(){
+    
+	  var $container = $('#image-container');
+$container.imagesLoaded(function(){
+  $container.masonry({
+    itemSelector : '.field-item',
+    columnWidth : 1,
+	animationOptions : { queue: true, duration: 500 },
+	isAnimated : true,
+	
+  });
+});
+    
+    $container.infinitescroll({
+      navSelector  : '#page-nav',    // selector for the paged navigation 
+      nextSelector : '#page-nav a',  // selector for the NEXT link (to page 2)
+      itemSelector : '.field-item',     // selector for all items you'll retrieve
+      loading: {
+          finishedMsg: 'No more pages to load.',
+          img: 'http://i.imgur.com/6RMhx.gif'
+        }
+      },
+      // trigger Masonry as a callback
+      function( newElements ) {
+        // hide new items while they are loading
+        var $newElems = $( newElements ).css({ opacity: 0 });
+        // ensure that images load before adding to masonry layout
+        $newElems.imagesLoaded(function(){
+          // show elems now they're ready
+          $newElems.animate({ opacity: 1 });
+          $container.masonry( 'appended', $newElems, true ); 
+        });
+      }
+    );
+    
+  });
+  </script>
 </body>
