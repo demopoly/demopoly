1920f10cf6446c0857338f1b2da6b6356f768fad
root <root@DanielKeyTec-Linux.(none)>
2012-08-31 17:39:32 +0200

    Started updating CSS

--- drupal/sites/all/themes/demopoly/page.tpl.php
+++ drupal/sites/all/themes/demopoly/page.tpl.php
@@ -1,101 +1,73 @@
-
 <?php 
-$base_path = '/'.drupal_get_path('theme', 'demopoly').'/';
-
+	$base_path = '/'.drupal_get_path('theme', 'demopoly').'/';
+	if($user->uid==1){print render($tabs);}
 ?>
-<?php if ($user->uid==1):?>
-<?php print render($tabs);?>
-<?php endif;?>
 
-<div
-	id="wrapper">
-
-	<!-- Start Logo-->
-
-	<div id="logo">
-		<a class="logoLink" href="<?php print url('<front>')?>"
-			title="demopoly"><img
-			src="<?php print $base_path;?>img/logo-demopoly.png" alt="demopoly">
-		</a>
+<div class="wrapper">
+	<!-- Start Header -->
+	<div class="header">
+		<!-- Start Logo-->
+		<div class="logo">
+			<a class="logoLink" href="<?php print url('<front>')?>" title="demopoly">
+				<img src="<?php print $base_path;?>img/logo-demopoly.png" alt="demopoly">
+			</a>
+		</div>
+		<!-- Ende Logo-->
+				
+		<!-- Start Navigation-->
+		<div class="navigation">
+			<ul>
+				<?php if ($user->uid==0){?>
+				<li><a class="active login-button" href="#" target="_self" title="Login">Login</a></li>
+				<?php } else {?>
+				<li><a class="active profile-button" href="<?php print url('user')?>" target="_self" title="Logout">profile-settings</a></li>
+				<li><a class="active logout-button" href="<?php print url('user/logout')?>" target="_self" title="Logout">Logout</a></li>
+				<?php }?>
+			</ul>
+			<?php echo render(  menu_tree('main-menu'));?>
+		</div>
+		<!-- Ende Navigation-->
+		
+		<!-- Start Information -->
+		<div class="subnavigation">
+			<ul>
+				<li><a class="info" href="/information" target="_self" title="Information"></a></li>
+				<?php if($user->uid==0){?>
+				<li><a class="take-part" href="<?php print url('user/register')?>" target="_self" title="Take Part"></a></li>
+				<?php } ?>
+				<?php if($user->uid!=0){?>
+				<li><a class="leave" href="<?php print url('user/logout')?>" target="_self" title="Leave"></a></li>
+				<?php }?>
+			</ul>
+		</div>
+		<?php print render($page['login']);?>
+		<!-- Ende Information -->
+		
+		<!-- Start Claim -->
+		<div class="claim">
+			<p>this is a democratic art project</p>
+		</div>
+		<!-- Ende Claim -->
 	</div>
-
-	<!-- Ende Logo-->
-
-
-	<!-- Start Navigation-->
-
-	<div id="navigation">
-
-		<ul>
-			<?php if ($user->uid==0):?>
-			<li><a class="active login-button" href="#" target="_self"
-				title="Login">Login</a></li>
-			<?php else:?>
-			<li><a class="active profile-button" href="<?php print url('user')?>"
-				target="_self" title="Logout">profile-settings</a></li>
-			<li><a class="active logout-button"
-				href="<?php print url('user/logout')?>" target="_self"
-				title="Logout">Logout</a></li>
-
-			<?php endif;?>
-
-		</ul>
-		<?php echo render(  menu_tree('main-menu'));?>
-	</div>
-
-	<!-- Ende Navigation-->
-
-	<!-- Start Information -->
-
-	<div id="information">
-		<ul>
-			<li><a class="info" href="/information" target="_self"
-				title="Information"></a></li>
-			<?php if ($user->uid==0):?>
-			<li><a class="take-part" href="<?php print url('user/register')?>"
-				target="_self" title="Take Part"></a></li>
-			<?php endif;?>
-			<?php if ($user->uid!=0):?>
-			<li><a class="leave" href="<?php print url('user/logout')?>"
-				target="_self" title="Leave"></a></li>
-			<?php endif;?>
-		</ul>
-	</div>
-	<?php print render($page['login']);?>
-	<!-- Ende Information -->
-
-	<!-- Start Claim -->
-
-	<div id="claim">
-		<p>this is a democratic art project</p>
-	</div>
-
-	<!-- Ende Claim -->
-
-	<!--  start Content-->
+	<!-- Ende Header -->
+	
+	<!--  Start Content-->
 	<div id="content">
-		<?php if ($messages):?>
+		<?php if ($messages){?>
 		<div class="clearfix">
 			<?php print $messages;?>
 		</div>
-		<?php endif;?>
-		<?php if ($title):?>
+		<?php } ?>
+		<?php if($title){?>
 		<h1>
 			<?php print $title;?>
 		</h1>
-		<?php endif;?>
+		<?php } ?>
 		<?php print render($page['content']);?>
 	</div>
-
-
 	<!-- Ende Content -->
-
 </div>
-<nav id="page-nav">
-	<a href="index.php"></a>
-</nav>
+<!-- Ende Wrapper -->
 <script>
-
-
-  </script>
+</script>
 </body>
